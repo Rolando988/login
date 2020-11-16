d560b32b6541dcc7336b06efb5efb7cf10b55448
Rolando988 <68891894+Rolando988@users.noreply.github.com>
2020-11-15 22:31:49 -0600

    Updates

---
+++ precache-manifest.59e449ba2f1d9b1ebb1b4893a5c69404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac6ec4132393f61f8f495c481bc044ec",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "e6b05438a7de7d399d19",
+    "url": "/login/static/css/main.3d592f50.chunk.css"
+  },
+  {
+    "revision": "397b25d8920e06d49c3a",
+    "url": "/login/static/js/2.fca73c50.chunk.js"
+  },
+  {
+    "revision": "608ba5ece62cd2c14649ef57eba241d6",
+    "url": "/login/static/js/2.fca73c50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6b05438a7de7d399d19",
+    "url": "/login/static/js/main.b48e45e0.chunk.js"
+  },
+  {
+    "revision": "8fa4d3be2c1cc49fb118",
+    "url": "/login/static/js/runtime-main.3bac6516.js"
+  }
+]);
